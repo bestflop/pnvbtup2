f305b342ec48393273dd90de9144dda3824fe69e
yeuboth <121703013+yeuboth@users.noreply.github.com>
2023-01-04 07:56:11 +0700

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -163,7 +163,7 @@ def fshare(url: str) -> str:
 def fourshare(url: str) -> str:
     fileid =  url.split('https://4share.vn/f/')[1]
     header = {
-            "accesstoken01": TOKENBONSHARE,
+            "accesstoken01": "TK_47363f3733232c7e203d7e69737e7577717f",
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
             "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             "content-type": "application/x-www-form-urlencoded",
